40aaa6d4dd2424dd2e1e84aab559a50b970adf22
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-03-24 11:37:29 +0300

    feat: added static files

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const aiRoutes = require("./routes/aiRoutes");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const path = require('path');
+
+const aiRoutes = require('./routes/aiRoutes');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/myai", aiRoutes);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// SERVING STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/myai', aiRoutes);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 const port = 3000;
 app.listen(port, () => {
-  console.log("port is running on ");
+  console.log(`http://localhost:${port}`);
 });
